61133e4b394c69da2a4b544b24f4cec093d115d9
GitHub Action <action@github.com>
2023-04-14 14:46:28 +0000

    import v2.2.7

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -4,7 +4,7 @@
 
 /* These will change with each release of LibreSSL-portable */
 #define LIBRESSL_VERSION_NUMBER	0x20020006L
-#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.2.6"
+#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.2.7"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
