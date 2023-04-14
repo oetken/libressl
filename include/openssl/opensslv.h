8d36a8e8a296b42ffbf190345fea0f57708c8cdb
GitHub Action <action@github.com>
2023-04-14 14:46:36 +0000

    import v2.3.10

--- include/openssl/opensslv.h
+++ include/openssl/opensslv.h
@@ -1,10 +1,10 @@
-/* $OpenBSD: opensslv.h,v 1.31 2015/08/20 00:15:32 bcook Exp $ */
+/* $OpenBSD: opensslv.h,v 1.33.2.7 2016/10/02 23:24:56 bcook Exp $ */
 #ifndef HEADER_OPENSSLV_H
 #define HEADER_OPENSSLV_H
 
 /* These will change with each release of LibreSSL-portable */
-#define LIBRESSL_VERSION_NUMBER	0x20030001L
-#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.3.1"
+#define LIBRESSL_VERSION_NUMBER	0x2030a00fL
+#define LIBRESSL_VERSION_TEXT	"LibreSSL 2.3.10"
 
 /* These will never change */
 #define OPENSSL_VERSION_NUMBER	0x20000000L
